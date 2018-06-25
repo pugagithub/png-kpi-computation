0c9a4d1c49c0d3a56abace04a74824c427834f58
Pugazh <pugal.sym@gmail.com>
2018-06-25 11:31:37 +0530

    Added Unit Tests

--- src/test/java/com/png/kpicomputation/controller/KPIComputationControllerTest.java
+++ src/test/java/com/png/kpicomputation/controller/KPIComputationControllerTest.java
@@ -1,9 +1,12 @@
 package com.png.kpicomputation.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(BlockJUnit4ClassRunner.class)
 public class KPIComputationControllerTest {
 
     @Test
